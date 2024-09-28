0f8b7674903978cb78017f223d7d24851c82c12c
Ephraim A. <126341856+tekika1@users.noreply.github.com>
2024-09-28 13:10:38 +0100

    created the frame for the game

--- Flappy-bird/src/App.java
+++ Flappy-bird/src/App.java
@@ -1,5 +1,16 @@
+import javax.swing.*;
+
 public class App {
     public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
+      int boardWidth = 360;
+      int boardHeight = 640;
+
+      JFrame frame = new JFrame("Flappy Bird");
+      frame.setVisible(true);
+      frame.setSize(boardWidth,boardHeight);
+      frame.setLocationRelativeTo(null);
+      frame.setResizable(false);
+      frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+
     }
 }
